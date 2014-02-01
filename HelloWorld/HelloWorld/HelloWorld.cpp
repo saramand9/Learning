38cd24334290c18f983ea750a7d2987bb7264fd3
saramand9 <saramand9@gmail.com>
2014-02-01 09:14:47 +0800

    C++ 11
    HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,14 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	auto str = "hello world";
+	printf("%s\n", str);
+
+	return 0;
+}
+
